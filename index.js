e537d74adf5b5bac66484918b1e98b4ccf1146e5
Naheed Awan <nadsawan@yahoo.com>
2022-02-14 16:17:53 -0500

    Adding places to controller

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config()
 const express = require ('express')
 const app = express()
+// importing router
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req,res)=>{
     res.send('Hello World')
 })
-app.listen(3000)
+// creating 404 route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+app.listen(process.env.PORT)
