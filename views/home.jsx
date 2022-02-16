92b65697aca242a1c9946a26c165b52a57eff566
Naheed Awan <nadsawan@yahoo.com>
2022-02-16 11:17:06 -0500

    Adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -4,7 +4,7 @@ function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div><img src="/images/peach-oatmeal1.jpg" alt="Peach Oatmeal bowl" /></div>
                 <div>Photo by <a href="https://unsplash.com/@alexandermils?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alexander Mils</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                 </div>
