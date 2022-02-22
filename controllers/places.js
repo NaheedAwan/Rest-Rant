4e64902f62484d0f0fedd9130d91f9e5a7bb2d84
Naheed Awan <nadsawan@yahoo.com>
2022-02-21 21:47:10 -0500

    adding delete and add

--- controllers/places.js
+++ controllers/places.js
@@ -3,19 +3,7 @@ const places = require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-// Edit
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
+
 // show
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -29,7 +17,38 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id })
   }
 })
+// New
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+// put route
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
 
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+// post Route
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
@@ -45,6 +64,19 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+// Edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id],id })
+  }
+}) 
 // delete
 router.delete('/places/:id', (req, res) => {
   let id = Number(req.params.id)
